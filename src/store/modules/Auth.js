86179962a3ac7c1aad638b5e1afb17d213f2806b
longmaikel <kutaj74@gmail.com>
2020-09-11 14:33:07 +0200

    setup, created base files

---
+++ src/store/modules/Auth.js
@@ -0,0 +1,22 @@
+const state = {
+    user: 'root',
+    password: 'secret',
+    logged: false
+};
+
+const getters = {};
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
