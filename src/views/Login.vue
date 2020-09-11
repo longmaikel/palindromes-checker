152a858e90b166bf468d4acf9e9bab94e2c3392c
longmaikel <kutaj74@gmail.com>
2020-09-11 14:41:09 +0200

    renamed input name

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>Login</h1>
     <form action="">
-      <input type="text" name="uer" placeholder="Login">
+      <input type="text" name="user" placeholder="Login">
       <input type="password" name="password" placeholder="Password">
       <button type="submit">Sign In</button>
     </form>
