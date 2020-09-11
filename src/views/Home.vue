7f9ee41713a4de3ebe16b827a1e725845145094b
longmaikel <kutaj74@gmail.com>
2020-09-11 16:39:39 +0200

    use palindrome components in view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
     <h1>Palindromes checker</h1>
+    <palindrome-form></palindrome-form>
+    <palindromes-list></palindromes-list>
   </div>
 </template>
 
 <script>
-
+import PalindromeForm from "@/components/PalindromeForm";
+import PalindromesList from "@/components/PalindromesList";
 export default {
   name: "Home",
   components: {
-
+    PalindromeForm,
+    PalindromesList
   }
 };
 </script>
