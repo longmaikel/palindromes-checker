57ae09a80e09858cd3bd190897debd0ebcff8545
longmaikel <kutaj74@gmail.com>
2020-09-11 16:34:56 +0200

    created palindrome form and palindrome list components

---
+++ src/components/PalindromeForm.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="palindrome-form">
+  <form @submit.prevent="checkPalindrome">
+    <span class="info">{{ info }}</span>
+    <input type="text" name="palindrome" placeholder="Palindrome" v-model="palindrome">
+    <button type="submit">Check</button>
+  </form>
+</div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      palindrome: null,
+      message: null
+    };
+  },
+  methods: {
+    checkPalindrome() {
+      this.$store.dispatch('checkPalindrome', this.palindrome)
+          .then(() => {
+            this.$router.push({name: "Home"})
+          })
+          .catch(error => {
+            this.error = error;
+          });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
