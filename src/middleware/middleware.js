8e64912f0803fda4fdec68a6cce17ede64006450
longmaikel <kutaj74@gmail.com>
2020-09-12 09:58:09 +0200

    format code

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,15 +1,13 @@
 import store from "@/store/index";
 
 const loginMiddleware = (to, from, next) => {
-    if (store.getters.logged) {
-        next();
-        return;
-    } else {
-        next({name: "Login"});
-        return;
-    }
-}
+  if (store.getters.logged) {
+    next();
+    return;
+  } else {
+    next({ name: "Login" });
+    return;
+  }
+};
 
-export {
-    loginMiddleware
-}
+export { loginMiddleware };
